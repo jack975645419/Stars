7190abe51b8502dc0915696dadf62f25f8a58c40
Jack_in_Xiaojiang <jack13726560352@126.com>
2019-10-25 23:40:27 +0800

    创立之初
    起初，神创造天地。

---
+++ StarWorld/server/src/common.ts
@@ -0,0 +1,39 @@
+
+class common extends laya.cloud.ServerScriptBase implements laya.cloud.IServerScript{
+    oncreated():void 
+    {
+        console.log("js create room");
+        console.log("   name="+this.name);
+        console.log("   master="+this.master);
+        console.log("   fps="+this.fps);
+        console.log("   duration="+this.duration);
+        console.log("   usernum="+this.usernum);
+    }
+
+    onstart():void 
+    {
+        console.log("js Room '"+this.name+"' Start!!");
+    }
+
+    onclose():void 
+    {
+        console.log("js Room '"+this.name+"' Closed!!");
+    }
+
+    onuserin(userid:string, data:Object):void 
+    {
+        
+    }
+
+    onuserout(userid:string):void 
+    {
+        console.log("js Room '"+this.name+"' user logout, userid='"+userid+"'");
+    }
+
+    onuserevent(userid:string, key:string, val:string):void 
+    {
+        console.log("js Room '"+this.name+"' recv user '"+userid+"' event, key='"+key+"' value='"+val+"'");
+    }
+    
+    onupdate():void{}
+}
